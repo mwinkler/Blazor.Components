2162b03412d82d503cbd3b7d74b9655601269101
Matthias Winkler <mwinkler@gmx.ch>
2018-10-22 11:08:54 +0200

    First implementation PagePanel

---
+++ src/PagePanelComponent/PageState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PagePanelComponent
+{
+    public struct PageState
+    {
+        public PageState(string width, string left)
+        {
+            Width = width;
+            Left = left;
+        }
+
+        public string Width { get; set; }
+        public string Left { get; set; }
+    }
+}
